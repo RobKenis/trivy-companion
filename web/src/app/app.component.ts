b6278c952117c410fa7814e960b8edf577f2eae1
Rob Kenis <rob.kenis@hotmail.com>
2024-06-17 21:58:09 +0200

    Visualize scanned images in table

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,13 +1,51 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatTableModule} from '@angular/material/table';
+import {HttpClient} from '@angular/common/http';
+
+interface Origin {
+  container: string;
+  namespace: string;
+}
+
+interface Artifact {
+  repository: string;
+  tag: string;
+  digest: string;
+  registry: string;
+}
+
+interface ScanMetadata {
+  origin: Origin;
+  artifact: Artifact;
+}
+
+export interface VulnerabilityReport {
+  name: string;
+  scanMetadata: ScanMetadata;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatTableModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'trivy-companion';
+  displayedColumns: string[] = ['name', 'image'];
+
+  reports: VulnerabilityReport[] = [];
+  
+  loadReports = () => {
+    this.http.get('http://localhost:8080/vulnerability-reports')
+      .subscribe((data: any) => {
+        this.reports = data;
+      });
+  }
+
+  constructor(private http: HttpClient) {
+    this.loadReports();
+  }
 }
